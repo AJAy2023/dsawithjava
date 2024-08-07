7e1b2116a580ed6f9b9375e43f1b9ba7dbfe1069
Ajay Jadhav <115634272+AJAy2023@users.noreply.github.com>
2024-08-07 18:03:48 +0000

    leftside pattern

---
+++ basicjava/leftside.java
@@ -0,0 +1,15 @@
+public class leftside {
+    public static void main(String[] args) {
+        int n=4; 
+        int nst=1;
+        for(int i=1; i<=n; i++)
+        {
+            for(int k=1; k<=nst; k++)
+            {
+                System.out.print("*");
+            }
+              System.out.println();
+              nst++;
+        }
+    }
+}
