cf69d8faa02fe02e07a157391eacfeab4502da9a
Ajay Jadhav <115634272+AJAy2023@users.noreply.github.com>
2024-08-16 17:27:38 +0000

    condtion_02

---
+++ function_program's /condition_02.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class condition_02 {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner  tc = new Scanner(System.in);
+        int n = tc.nextInt();
+        if(n>=10000 )
+        {
+            System.out.println("goa");
+        }
+        else
+        {
+             System.out.println("murthal");
+        }
+    }
+}
